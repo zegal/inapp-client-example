fa70805eb8a2f82f573ebaced545be0070693c95
ankita.sharma@zegal.com <ankita.sharma@zegal.com>
2020-03-10 12:33:07 +0545

    Add document model for the portal

---
+++ models/document.model.js
@@ -0,0 +1,16 @@
+"use strict";
+var mongoose = require("mongoose");
+
+var Document = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    json: { type: mongoose.Schema.Types.Mixed },
+  },
+  {
+    _id: true,
+    timestamps: true,
+    collection: "keys",
+  }
+);
+
+module.exports = mongoose.model("Document", Document);
