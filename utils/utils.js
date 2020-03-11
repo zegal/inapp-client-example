76b17b80d56339cbea71e9c7b428f1906af2ce03
nishlashakya <nishla.shakya@dragonlaw.com.hk>
2020-03-11 14:13:06 +0545

    updating doctype id

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
 async function createDocumentHandler() {
 	const doctypePayload = {
-		doctype: "5e439341a4cb1c531292ca7b",
+		doctype: "5e5f3ade436e69c4ad043b0b",
 		guide: "5db157dd545208010092943a",
 		title: document.getElementById("docTitle").value || 'New document',
 	}
