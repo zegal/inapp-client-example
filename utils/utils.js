e5559ef07b630e73a331cd1329baadbfb9138421
nishlashakya <nishla.shakya@dragonlaw.com.hk>
2020-03-11 13:32:31 +0545

    title render+ design update for modal

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@ async function createDocumentHandler() {
 	const doctypePayload = {
 		doctype: "5e439341a4cb1c531292ca7b",
 		guide: "5db157dd545208010092943a",
-		title: 'New document',
+		title: document.getElementById("docTitle").value || 'New document',
 	}
 	await zegal.createDocument(doctypePayload)
 };
