aa7b33a743136d2398a87d93e9262b85ed8ff315
nishlashakya <nishla.shakya@dragonlaw.com.hk>
2020-03-12 17:20:47 +0545

    updates in ui for testing options and signers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Api Portal" });
+  const zegalDoctypes = [
+    {
+      id: "5e58aa64356286efc1eec974",
+      display_name: "Cross Reference Testing Do not touch"
+    },
+    {
+      id: "5e58dc44d3ebe66752107268",
+      display_name: "agreement date testing"
+    },
+    {
+      id: "5e5f3ade436e69c4ad043b0b",
+      display_name: "All field type testing do not edit and save"
+    },
+  ]
+  res.render("index", { title: "API Portal", zegalDoctypes });
 });
 
 module.exports = router;
--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,46 @@
+let doctypePayload = {
+	doctype: "5e5f3ade436e69c4ad043b0b",
+	guide: "5db157dd545208010092943a",
+}
+let options = {}
+let user = {
+	access: {
+		type: "view",
+		invited: false
+	},
+	primary_name: {
+		given_name: "",
+		surname: ""
+	},
+	signature: {invoked: true },//signers
+	email: "",
+	role: "member",
+}
 async function createDocumentHandler() {
-	const doctypePayload = {
-		doctype: "5e5f3ade436e69c4ad043b0b",
-		guide: "5db157dd545208010092943a",
-		title: document.getElementById("docTitle").value || 'New document',
+	user = {
+		...user,
+		primary_name: {
+			given_name: document.getElementById("given_name").value,
+			surname: document.getElementById("surname").value
+		},
+		email: document.getElementById("email").value
 	}
-	await zegal.createDocument(doctypePayload)
+	doctypePayload = {
+		...doctypePayload,
+		title: document.getElementById("docTitle").value || 'New document',
+		users: document.getElementById('addSigners').checked ? [user]: []
+	}
+	options = {
+		disableSignerAdd: !document.getElementById('addSigners').checked,
+		disablePreview: !document.getElementById('enablePreview').checked,
+		disableDownload: !document.getElementById('enableDownload').checked
+	}
+	
+	document.getElementById('modalTitle').innerHTML = doctypePayload.title
+	await zegal.createDocument(doctypePayload, options)
 };
+
+
+const selectDoctype = (id) => {
+	doctypePayload.doctype = id
+}
