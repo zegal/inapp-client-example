056ea74d9d1659321dfc8d14421bfee5933a47e5
ankita.sharma@zegal.com <ankita.sharma@zegal.com>
2020-04-21 12:17:52 +0545

    Remove config checking and set it for local only

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
-let env
-if (process.env.NODE_ENV === 'development') {
-  env = require('dotenv').config();
-} else if (process.env.NODE_ENV === 'staging') {
-  env = require('dotenv').config({ path: 'staging.env' });
-} else if (process.env.NODE_ENV === 'production') {
-  env = require('dotenv').config({ path: 'prod.env' });
+let env = process.env;
+if (process.env.DEBUG_MODE === true || process.env.DEBUG_MODE === "true") {
+  // in the deployment, we'll always use main env. this below is for local debug mode
+  if (process.env.NODE_ENV === "development") {
+    env = require("dotenv").config();
+  } else if (process.env.NODE_ENV === "staging") {
+    env = require("dotenv").config({ path: "staging.env" });
+  } else if (process.env.NODE_ENV === "production") {
+    env = require("dotenv").config({ path: "prod.env" });
+  }
 }
 module.exports = {
   ...env.parsed,
