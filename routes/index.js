e0b74e22b62bfca39bac5671cbc299700c050dc9
nishlashakya <nishla.shakya@dragonlaw.com.hk>
2020-09-29 16:33:30 +0545

    refactor

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const DocumentService = require("../services/document/document.service");
 /* GET home page. */
 router.get("/", async function(req, res, next) {
   const documents = await DocumentService.getAllDocuments();
+
   res.render("index", {
     title: "API Portal",
     zegalClient: config.ZEGAL_CLIENT,
-    key: "pk_8e367994-27df-43ee-8a16-f0fb1c513ce3",
-    // key: config.CLIENT_KEY,
+    key: config.CLIENT_KEY,
     signers: [],
-    documents
+    documents,
   });
 });
 
