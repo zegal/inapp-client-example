16925025783c3f626e1f04ca80d6a33ef6cf8304
nishlashakya <nishla.shakya@dragonlaw.com.hk>
2020-09-29 16:37:14 +0545

    refactor

--- libs/db.js
+++ libs/db.js
@@ -10,22 +10,22 @@ mongoose.connect(config.MONGO_SERVER, {
 
 const db = mongoose.connection;
 // handling events
-db.on("error", function (err) {
+db.on("error", function(err) {
   // exit on error
   console.log(err, "Mongo connection error");
   process.exit(0);
 });
-db.on("connecting", function () {
+db.on("connecting", function() {
   console.log("Connecting to MongoDB...");
 });
 
-db.on("connected", function () {
+db.on("connected", function() {
   console.log("Connected to MongoDB!");
 });
-db.on("reconnected", function () {
+db.on("reconnected", function() {
   console.log("MongoDB reconnected!");
 });
 
-db.on("disconnected", function () {
+db.on("disconnected", function() {
   console.log("MongoDB disconnected!");
 });
